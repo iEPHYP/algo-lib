648d7b9199889be00e33f0ae4cfcf24bb0f852b2
iEPHYP <eric.maulabi@gmail.com>
2024-01-15 01:31:05 +0500

    fix: fixed jest root imports

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,12 @@
 module.exports = {
   preset: 'ts-jest',
+  moduleNameMapper: {
+    '^([^./]+)$': '<rootDir>/src/$1',
+    '^([^./]+)/(.+)$': '<rootDir>/src/$1/$2',
+  },
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
 };
