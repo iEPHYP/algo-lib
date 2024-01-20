3a31f6bc3e4d7a15b545c9056ff254549e9e0da5
iEPHYP <eric.maulabi@gmail.com>
2024-01-20 18:50:21 +0500

    feat: added sliding window technique problem

---
+++ src/dynamic-programming/sliding-window-technique/series-sums/README.md
@@ -0,0 +1,1 @@
+Need to find a sum of the k series elements in the array for all cases.
---
+++ src/dynamic-programming/sliding-window-technique/series-sums/series-sums.ts
@@ -0,0 +1,3 @@
+// TODO: add realization using the sliding window technique
+// S(i) = S(i-1) - a(i-1) + a(i + k - 1)
+// Complexity O(k + n)
